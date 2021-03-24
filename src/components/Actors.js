53b756baf2a938b27e7c70868babc6d1f81ff506
Trayshawn <traytwebb14@yahoo.com>
2021-03-24 18:27:18 -0500

    commit tree 2a87c384006e46cfccfa81922556bad3a2bfa988

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {actors.map(actor => { return (
+          <div>
+            <h2>Name: {actor.name}</h2>
+            <p> Movies: </p>
+            <p>
+              <ul>
+                {actor.movies.map(movie => {
+                  return <li>{movie}</li>
+                  })}
+              </ul>
+            </p>
+
+          </div>
+        )})}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie => { return (
+          <div>
+            <h2>Name: {movie.title}</h2>
+            <p> Time: {movie.time}</p>
+            <p> Genres: </p>
+            <p>
+              <ul>
+                {movie.genres.map(genre => {
+                  return <li>{genre}</li>
+                  })}
+              </ul>
+            </p>
+
+          </div>
+        )})}
+        
     </div>
   );
 };
